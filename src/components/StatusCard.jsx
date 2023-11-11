8a385eb458a5532f6daf215eac9745d3abfd393d
Jargua <urielsilvag@outlook.com>
2023-11-10 21:10:30 -0600

    fix: Colores de los circulos

--- src/components/StatusCard.jsx
+++ src/components/StatusCard.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react"
+
+const classColors = {
+  red: 'bg-red-400',
+  green: 'bg-green-400'
+}
 export function StatusCard({ data, title }) {
   return (
     <div className='bg-slate-700 rounded-lg w-[300px] h-[150px] flex flex-col justify-center items-center justify-self-center gap-2'>
-      <div className={`bg-red-400 bg-green-400 bg-${data ? `green` : `red`}-400 rounded-full w-12 h-12`}>
+      <div className={`${data ? classColors.green : classColors.red} rounded-full w-12 h-12`}>
     
       </div>
+      <div className="bg-red-400"></div>
+      <div className="bg-green-400"></div>
       <span className="font-bold">{title}</span>
     </div>
   )
